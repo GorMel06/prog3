d679c45ceeac662c947f5f4da5de6bfb787b8bd3
Mkit5 <edvard.grigoryan1.y@tumo.org>
2022-03-26 10:58:22 +0400

    das6

--- MeloyanGorLevel2/script.js
+++ MeloyanGorLevel2/script.js
@@ -1,11 +1,4 @@
-var matrix = [];
-
-var flouwerArr = [];
-var grassArr = [];
-var grassEaterArr = [];
-var gishatichArr = [];
-var axjikArr = [];
-
+var socket = io();
 
 var side = 20;
 
@@ -21,31 +14,7 @@ function setup() {
     createCanvas(matrix[0].length * side, matrix.length * side);
     background('#acacac');
 
-    for (var y = 0; y < matrix.length; y++) {
-        for (var x = 0; x < matrix[y].length; x++) {
-
-            if (matrix[y][x] == 1) {
-                var gr = new Grass(x, y, 1);
-                grassArr.push(gr);
-            }
-            else if (matrix[y][x] == 2) {
-                var ge = new GrassEater(x, y, 2);
-                grassEaterArr.push(ge);
-            }
-            else if (matrix[y][x] == 3) {
-                var fl = new Flouwer(x, y, 3);
-                flouwerArr.push(fl);
-           }
-            else if (matrix[y][x] == 4) {
-                var gi = new Gishatich(x, y, 4);
-                gishatichArr.push(gi);
-            }
-            else if (matrix[y][x] == 5) {
-                var ax = new Axjik(x, y, 5);
-                axjikArr.push(ax);
-            }
-        }
-    }
+  
 }
 
 function draw() {
@@ -79,20 +48,5 @@ function draw() {
             }
         }
     }
-
-    for (var i in grassArr) {
-        grassArr[i].mul();
-    }
-    for (var i in grassEaterArr) {
-        grassEaterArr[i].eat();
-    }
-    for (var i in gishatichArr) {
-        gishatichArr[i].eat();
-    }
-    for (var i in axjikArr) {
-        axjikArr[i].eat();
-    }
-    for(var i in gishatichArr) {
-        gishatichArr[i].die();
-    }
+    socket.on('send matrix', draw)
 }
--- MeloyanGorLevel2/server.js
+++ MeloyanGorLevel2/server.js
@@ -1,4 +1,3 @@
-//առաջին 10 տողը նույնությամբ գրիր, որպեսզի լոկալհոստ ունենաս
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -12,107 +11,103 @@ app.get('/', function (req, res) {
 });
 server.listen(3000);
 
-//10
+var matrix = [];
+var flouwerArr = [];
+var grassArr = [];
+var grassEaterArr = [];
+var gishatichArr = [];
+var axjikArr = [];
 
-//քո սկրիպտ ֆայլից տպի մատրիցդ գեներացնոլու հատվածը և դատարկ զանգվածը
-// ինձ մոտ այն չի գեներացվում,,,քեզ մոտ լաաաավ կլինի , որ գեներացվի
+var n = 50;
 
- matrix = [[2, 2, 1, 2, 1, 0, 1],
-[1, 2, 1, 0, 1, 1, 1],
-[1, 0, 2, 0, 1, 0, 1],
-[0, 1, 0, 1, 0, 0, 1],
-[1, 2, 1, 0, 1, 1, 1],
-[1, 0, 2, 0, 1, 0, 1],
-[0, 1, 0, 1, 0, 1, 1],
-[1, 2, 1, 0, 1, 1, 1],
-[1, 0, 2, 0, 1, 0, 1],
-[0, 1, 0, 1, 0, 0, 1],
-[1, 2, 1, 0, 1, 1, 1],
-[1, 0, 2, 0, 1, 0, 1],
-[0, 1, 0, 1, 0, 0, 1]]
+Grass = require("./Grass")
+GrassEater = require("./GrassEater")
+Axjik = require("./Axjik")
+Flouwer = require("./Flouwer")
+Gishatich = require("./Gishatich")
 
-//այստեղ քո պատրաստի թվերով լցված զանգվածը ուղարկում ես կլիենտին:
-//սոքեթի emit մեթոդը թույլ է տալիս առաջին արգումենտով ստեղծել իվենթի անունը, 
-//2-րդ արգումենտով ուղղարկել տվյալը, այն ինչ ուզում ես ուղարկել
+function rand(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
-    io.sockets.emit('send matrix', matrix)
-    
-// հիմա գնա կլիենտի ֆայլ
+for (let i = 0; i < n; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < n; j++) {
+        matrix[i][j] = Math.floor(rand(0, 3))
 
-//.........................................լոադինգ
+    }
+}
 
-//եթե գնացիր ու ամենինչ գրեցիր, արի էստեղ, դեռ անելիք ունենք
+io.sockets.emit('send matrix', matrix)
 
-//էստեղ բեր քո գազանիկների դատարկ զանգվածները
-    grassArr = [];
-    grassEaterArr = []
 
-    //քանի որ քո կլասս-երը արդեն մոդուլներ են և ոչ մի կապ չունեն html ֆայլիդ հետ՝
-    //այլ աշխատում են սերվերի վրա:
-    //Դու պետք է նրանց իմպորտ անես: Ինձ մոտ նրանք երկուսն են, քեզ մոտ ավելի շատ
-     Grass = require("./Grass")
-     GrassEater = require("./GrassEater")
 
-    //Այժմ լցնենք մատրիցը օբյեկտներով
-    //սարքի մի հատ ֆունկցիա օրինակ createObject անունով
-    //և էստեղ բեր քո սկրիպտ ֆայլի օբյեկտներով լցնող հատվածը
-    function createObject(matrix) {
-        for (var y = 0; y < matrix.length; y++) {
-            for (var x = 0; x < matrix[y].length; x++) {
-                if (matrix[y][x] == 1) {
-                    var gr = new Grass(x, y, 1);
-                    grassArr.push(gr)
-                }
-                else if (matrix[y][x] == 2) {
-                    var grEater = new GrassEater(x, y, 2);
-                    grassEaterArr.push(grEater)
+function createObject(matrix) {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
 
-                }
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y, 1);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                var ge = new GrassEater(x, y, 2);
+                grassEaterArr.push(ge);
+            }
+            else if (matrix[y][x] == 3) {
+                var fl = new Flouwer(x, y, 3);
+                flouwerArr.push(fl);
+            }
+            else if (matrix[y][x] == 4) {
+                var gi = new Gishatich(x, y, 4);
+                gishatichArr.push(gi);
+            }
+            else if (matrix[y][x] == 5) {
+                var ax = new Axjik(x, y, 5);
+                axjikArr.push(ax);
             }
         }
-        // և կրկին ուղարկի կլիենտիդ: 
-        //չմոռանաս , որ emit-ը տվյալ ուղարկողն է, իսկ on-ը ստացողը և կատարողը
-        //այս դեպքում 2-րդ արգումենտը տվյալն է
-        io.sockets.emit('send matrix', matrix)
-
-
     }
+    io.sockets.emit('send matrix', matrix)
+}
 
 
-    //հիմա անցնենք նրանց վայրենի գործունեությանը
-    //որևէ անունով կոչիր ֆունկցիադ և մեջը դիր մեթոդների հատվածը:
-
-    function game() {
-        for (var i in grassArr) {
-            grassArr[i].mul()
-        }
-        for (var i in grassEaterArr) {
-            grassEaterArr[i].eat();
-        }
-        //այո, դու ճիշտ ես տեսնում, կրկին և կրկին
-        io.sockets.emit("send matrix", matrix);
+function game() {
+    for (var i in grassArr) {
+        grassArr[i].mul();
     }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].eat();
+    }
+    for (var i in gishatichArr) {
+        gishatichArr[i].eat();
+    }
+    for (var i in axjikArr) {
+        axjikArr[i].eat();
+    }
+    for(var i in gishatichArr) {
+        gishatichArr[i].die();
+    }
+    io.sockets.emit("send matrix", matrix);
+}
 
-    //մեր խաղի շարժը լինելու է 1 վարկյանը մեկ
-    setInterval(game, 1000)
-    
+setInterval(game, 1000)
 
 
-      // մինչև այժմ մենք ինքներս էինք դնում իվենթների անուննները, 
-      //օրինակ send matrix կամ ըըը... էլ չկա :D
-      // էստեղ connection պատրասի իվենթի անուն է, որը աշխատում է այն ժամանակ, 
-      //երբ որևէ մեկը աշխատացնում է սերվերը՝ մտնում է սերվեր
-      //և մենք դեռ չէինք կանչել createObject ֆունկցիան
-      // էստեղ կկանչենք )))
-io.on('connection', function (socket) {
+
+io.on('connection', function () {
     createObject(matrix)
-})
+});
 
-//դե ինչ այսօր այսքանը:
+var statistics = {};
 
-//ինձ համար շատ կարևոր է , որ հենց դու շատ լավ հասկանաս էս 
-//ամենը ու լինես լավագույնը քո ընտրած ոլորտում:
-
-
-
-//Գիտեմ, որ լիիիիիքը սխալ կա մեջը: Դուք ճիշտը գրեք :PPPPP
+setInterval(function() {
+    statistics.grass = grassArr.length;
+    statistics.grassEater = grassEaterArr.length;
+    statistics.gishatic = gishatichArr.length;
+    statistics.axjik = axjikArr.length;
+    statistics.flouwer = flouwerArr.length;
+    fs.writeFile("statistics.json", JSON.stringify(statistics), function(){
+        console.log("send")
+    })
+},1000)
